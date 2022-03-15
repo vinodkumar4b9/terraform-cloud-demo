5bc7917e730c93dd0c5e446a3ebf945092cf0f81
vinodkumar4b9 <vinodkumar4b9@gmail.com>
2022-03-15 14:45:44 +0530

    Update c2-variables.tf

--- c2-variables.tf
+++ c2-variables.tf
@@ -2,11 +2,9 @@
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
-  default = ""
 }
 
 variable "instance_type" {
   description = "EC2 Instance Type - Instance Sizing"
   type = string
-  default = ""
 }
